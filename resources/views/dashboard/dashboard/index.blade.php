934a06cbb501a637303091cf9ac9ce750c244484
mohagungnursalim <mohagungnursalim@gmail.com>
2024-08-08 13:10:36 +0800

    feat: menambahkan total kasus & total atasan pada dashboard

--- resources/views/dashboard/dashboard/index.blade.php
+++ resources/views/dashboard/dashboard/index.blade.php
@@ -16,14 +16,14 @@
             <div class="card">
                 <div class="card-header p-3 pt-2">
                     <div
-                        class="icon icon-lg icon-shape bg-gradient-success shadow-success text-center border-radius-xl mt-n4 position-absolute">
-                        <span class="material-symbols-outlined fa-3x text-white">
-                            check
+                        class="icon icon-lg icon-shape bg-gradient-dark shadow-success text-center border-radius-xl mt-n4 position-absolute">
+                        <span class="material-symbols-outlined text-white" style="font-size: 34pt; width: 34pt;">
+                            cases
                         </span>
                     </div>
                     <div class="text-end pt-1">
-                        <p class="text-sm mb-0 text-capitalize text-bold text-dark">Agenda Terkirim</p>
-                        <h4 class="mb-0">{{ $agenda_terkirim }}</h4>
+                        <p class="text-sm mb-0 text-capitalize text-bold text-dark">Kasus</p>
+                        <h4 class="mb-0">{{ $data['total_kasus'] }}</h4>
                     </div>
                 </div>
                 <hr class="dark horizontal my-0">
@@ -37,14 +37,14 @@
             <div class="card">
                 <div class="card-header p-3 pt-2">
                     <div
-                        class="icon icon-lg icon-shape bg-gradient-dark shadow-dark text-center border-radius-xl mt-n4 position-absolute">
-                        <span class="material-symbols-outlined fa-3x text-white">
-                            schedule
+                        class="icon icon-lg icon-shape bg-gradient-danger shadow-dark text-center border-radius-xl mt-n4 position-absolute">
+                        <span class="material-symbols-outlined text-white" style="font-size: 34pt; width: 34pt;">
+                            supervisor_account
                         </span>
                     </div>
                     <div class="text-end pt-1">
-                        <p class="text-sm mb-0 text-capitalize text-bold text-dark">Agenda Belum Terkirim</p>
-                        <h4 class="mb-0">{{ $agenda_belum_terkirim }}</h4>
+                        <p class="text-sm mb-0 text-capitalize text-bold text-dark">Atasan</p>
+                        <h4 class="mb-0">{{ $data['total_atasan'] }}</h4>
                     </div>
                 </div>
                 <hr class="dark horizontal my-0">
@@ -58,14 +58,14 @@
             <div class="card">
                 <div class="card-header p-3 pt-2">
                     <div
-                        class="icon icon-lg icon-shape bg-gradient-primary shadow-primary text-center border-radius-xl mt-n4 position-absolute">
-                        <span class="material-symbols-outlined fa-3x text-white">
+                        class="icon icon-lg icon-shape bg-gradient-info shadow-primary text-center border-radius-xl mt-n4 position-absolute">
+                        <span class="material-symbols-outlined text-white" style="font-size: 34pt; width: 34pt;">
                             group
                         </span>
                     </div>
                     <div class="text-end pt-1">
-                        <p class="text-sm mb-0 text-capitalize text-bold text-dark">Total Jaksa</p>
-                        <h4 class="mb-0">{{ $total_jaksa }}</h4>
+                        <p class="text-sm mb-0 text-capitalize text-bold text-dark">Jaksa</p>
+                        <h4 class="mb-0">{{ $data['total_jaksa'] }}</h4>
                     </div>
                 </div>
                 <hr class="dark horizontal my-0">
@@ -78,14 +78,14 @@
             <div class="card">
                 <div class="card-header p-3 pt-2">
                     <div
-                        class="icon icon-lg icon-shape bg-gradient-info shadow-info text-center border-radius-xl mt-n4 position-absolute">
-                        <span class="material-symbols-outlined fa-3x text-white">
+                        class="icon icon-lg icon-shape bg-gradient-warning shadow-info text-center border-radius-xl mt-n4 position-absolute">
+                        <span class="material-symbols-outlined text-white" style="font-size: 34pt; width: 34pt;">
                             adaptive_audio_mic
                         </span>
                     </div>
                     <div class="text-end pt-1">
                         <p class="text-sm mb-0 text-capitalize text-bold text-dark">Total Saksi</p>
-                        <h4 class="mb-0">{{ $total_saksi }}</h4>
+                        <h4 class="mb-0">{{ $data['total_saksi'] }}</h4>
                     </div>
                 </div>
                 <hr class="dark horizontal my-0">
@@ -97,13 +97,6 @@
     </div>
 
     <div class="row mt-4">
-
-        <!-- Chart container -->
-        <div id="chart_terkirim_div"></div>
-        <div id="chart_belum_terkirim_div"></div>
-        <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-        <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-
         <!-- Chart container -->
         <div id="chart_terkirim_div"></div>
         <div id="chart_belum_terkirim_div"></div>
@@ -112,101 +105,92 @@
 
         <script type="text/javascript">
             // Load Google Charts
-            google.charts.load('current', {
-                'packages': ['corechart']
-            });
-
+            google.charts.load('current', {'packages':['corechart']});
+        
             // Draw the charts when Google Charts is loaded
             google.charts.setOnLoadCallback(initCharts);
-
+        
             function initCharts() {
                 drawTerkirimChart();
                 drawBelumTerkirimChart();
             }
-
-
+        
             function drawTerkirimChart() {
                 $.ajax({
-                    url: "{{ url('/agenda-terkirim-sesuai-jadwal') }}",
+                    url: "{{ url('/agenda-terkirim-sesuai-jadwal') }}", // jika menggunakan HTTPS => tambahkan "[], true"
                     dataType: "json",
-                    success: function (data) {
+                    success: function(data) {
                         var dataTable = new google.visualization.DataTable();
                         dataTable.addColumn('datetime', 'Tanggal Waktu');
                         dataTable.addColumn('number', 'Jumlah');
-
+        
                         var maxValue = 0;
-                        $.each(data, function (index, row) {
+                        var totalCount = 0;
+        
+                        $.each(data, function(index, row) {
                             var value = parseFloat(row.jumlah);
                             dataTable.addRow([new Date(row.tanggal_waktu), value]);
+                            totalCount += value; // Sum the values for total count
                             if (value > maxValue) {
                                 maxValue = value;
                             }
                         });
-
+        
                         var options = {
-                            title: 'Agenda Terkirim',
+                            title: 'Agenda Terkirim (' + totalCount + ')', // Display total count in the title
                             curveType: 'function',
-                            legend: {
-                                position: 'bottom'
-                            },
+                            legend: { position: 'bottom' },
                             vAxis: {
                                 viewWindow: {
                                     max: maxValue + 2
                                 }
                             }
                         };
-
-                        var chart = new google.visualization.AreaChart(document.getElementById(
-                            'chart_terkirim_div'));
+        
+                        var chart = new google.visualization.AreaChart(document.getElementById('chart_terkirim_div'));
                         chart.draw(dataTable, options);
                     }
                 });
             }
-
+        
             function drawBelumTerkirimChart() {
                 $.ajax({
-                    url: "{{ url('/agenda-belum-terkirim-sesuai-jadwal') }}",
+                    url: "{{ url('/agenda-belum-terkirim-sesuai-jadwal') }}", // jika menggunakan HTTPS => tambahkan "[], true"
                     dataType: "json",
-                    success: function (data) {
+                    success: function(data) {
                         var dataTable = new google.visualization.DataTable();
                         dataTable.addColumn('datetime', 'Tanggal Waktu');
                         dataTable.addColumn('number', 'Jumlah');
-
+        
                         var maxValue = 0;
-                        $.each(data, function (index, row) {
+                        var totalCount = 0;
+        
+                        $.each(data, function(index, row) {
                             var value = parseFloat(row.jumlah);
                             dataTable.addRow([new Date(row.tanggal_waktu), value]);
+                            totalCount += value; // Sum the values for total count
                             if (value > maxValue) {
                                 maxValue = value;
                             }
                         });
-
+        
                         var options = {
-                            title: 'Agenda Belum Terkirim',
+                            title: 'Agenda Belum Terkirim (' + totalCount + ')', // Display total count in the title
                             curveType: 'function',
-                            legend: {
-                                position: 'bottom'
-                            },
+                            legend: { position: 'bottom' },
                             vAxis: {
                                 viewWindow: {
                                     max: maxValue + 2
                                 }
                             }
                         };
-
-                        var chart = new google.visualization.AreaChart(document.getElementById(
-                            'chart_belum_terkirim_div'));
+        
+                        var chart = new google.visualization.AreaChart(document.getElementById('chart_belum_terkirim_div'));
                         chart.draw(dataTable, options);
                     }
                 });
             }
-
         </script>
-
-
-
-
-
     </div>
 
     @endsection
